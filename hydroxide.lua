ce6ce0b43494269cefe7a82513a3012e306278bb
Absu245 <89591046+Absu245@users.noreply.github.com>
2024-10-20 18:44:08 +0300

    Update hydroxide.lua

--- hydroxide.lua
+++ hydroxide.lua
@@ -2,63 +2,24 @@ local CoreGui = game:GetService("CoreGui")
 local UserInput = game:GetService("UserInputService")
 local HttpService = game:GetService("HttpService")
 task.spawn(function() --yeah anti adonis
-local g = getinfo or debug.getinfo
-local d = false
-local h = {}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Pixeluted/adoniscries/main/Source.lua"))()
 
-local x, y
+local badFunctions = {"Crash", "HardCrash", "GPUCrash", "RAMCrash", "KillClient", "SetFPS"} -- Functions to hook
 
-setthreadidentity(2)
-
-for i, v in getgc(true) do
-    if typeof(v) == "table" then
-        local a = rawget(v, "Detected")
-        local b = rawget(v, "Kill")
-    
-        if typeof(a) == "function" and not x then
-            x = a
-            
-            local o; o = hookfunction(x, function(c, f, n)
-                if c ~= "_" then
-                    if d then
-                        warn(`Adonis AntiCheat flagged\nMethod: {c}\nInfo: {f}`)
-                    end
-                end
-                
-                return true
+for i,v in pairs(getgc()) do 
+    if type(v) == "function" then
+        local info = debug.getinfo(v)
+        local functionName = info.name
+        
+        -- Hook crash/lag functions
+        if info.source:find('=.Core.Functions') and table.find(badFunctions, functionName) then
+            print("Hooked \"" .. functionName .. "\"")
+            hookfunction(v, function()
+                print("Stopped \"" .. functionName .. "\" from running")
             end)
-
-            table.insert(h, x)
-        end
-
-        if rawget(v, "Variables") and rawget(v, "Process") and typeof(b) == "function" and not y then
-            y = b
-            local o; o = hookfunction(y, function(f)
-                if d then
-                    warn(`Adonis AntiCheat tried to kill (fallback): {f}`)
-                end
-            end)
-
-            table.insert(h, y)
         end
     end
 end
-
-local o; o = hookfunction(getrenv().debug.info, newcclosure(function(...)
-    local a, f = ...
-
-    if x and a == x then
-        if d then
-            warn(`zins | adonis bypassed`)
-        end
-
-        return coroutine.yield(coroutine.running())
-    end
-    
-    return o(...)
-end))
-
-setthreadidentity(7)
 end)
 local oldmodule = getloadedmodules
 getloadedmodules = function(...)
